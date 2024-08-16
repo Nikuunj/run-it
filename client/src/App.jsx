dfbc78c4c76e4a6a28d2f47248dc124b8f4dd8fa
Nikunj <mnikunj262@gmail.com>
2024-08-16 22:50:41 +0530

    add vercel package

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import Header from './component/Header';
 import Footer from './component/Footer';
 import OnlyJs from './pages/OnlyJs';
@@ -19,6 +20,7 @@ function App() {
       </AppContext.Provider>
     </motion.div>
       <Footer />
+      <Analytics />
     </div>
   );
 
